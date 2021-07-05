09e4dca3e9debedc90065ab0d19293004788d6c1
Matthew Werner <matwer@outlook.com>
2021-07-04 21:37:38 -0700

    added the 100k and 1mth prime

--- howLong.js
+++ howLong.js
@@ -1,6 +1,6 @@
 // Original 
 Number.prototype.isPrime = function() {
-    for( let i=2; i<this; i++ ) {
+    for( let i=2; i < this; i++ ) {
         if( this % i === 0 ) {
             return false;
         }
@@ -20,10 +20,10 @@ while( primeCount < 1e4 ) {
 }
 
 console.log(`Original: the 10,000th prime number is ${num-1}`);
-console.log(`Original took ${performance.now() - start} milliseconds to run`);
+console.log(`Original took ${performance.now() - start} ms to run`);
 
 // Tweaked
-Number.prototype.tIsPrime = function() {
+Number.prototype.tweakedPrime = function() {
     for( let i=2; i <= Math.sqrt(this); i++ ) {
         if( this % i === 0 ) {
             return false;
@@ -36,18 +36,52 @@ const tStart = performance.now();
 let tPrimeCount = 0;
 let tNum = 2; // for math reasons, 1 is considered prime
 while( tPrimeCount < 1e4 ) {
-    if( tNum.tIsPrime() ) {
+    if( tNum.tweakedPrime() ) {
         tPrimeCount++;
     }
     tNum++;
 }
 console.log(`Tweaked: the 10,000th prime number is ${tNum-1}`);
-console.log(`Tweaked took ${performance.now() - tStart} milliseconds to run`);
+console.log(`Tweaked took ${performance.now() - tStart} ms to run`);
 
 // RESUlTS: 
-// Original took 42682.418399989605 miliiseconds to run
-// Tweaked took 278.4641000032425 miliiseconds to run
+// The 10,000th prime number is 104729
+// Original took 46575 miliiseconds to run
+// Tweaked took 293 miliiseconds to run
 
+const hunStart = performance.now();
+let hunPrimeCount = 0;
+let hunNum = 2; // for math reasons, 1 is considered prime
+while( hunPrimeCount < 1e5 ) {
+    if( hunNum.tweakedPrime() ) {
+        hunPrimeCount++;
+    }
+    hunNum++;
+}
+console.log(`Tweaked: the 100,000th prime number is ${hunNum-1}`);
+console.log(`Tweaked took ${performance.now() - hunStart} milliseconds to run`);
+
+// RESUlTS: 
+// The 100,000th prime number is 1,299,709
+// Tweaked took 9057 ms to run
+
+const milStart = performance.now();
+let milPrimeCount = 0;
+let milNum = 2; // for math reasons, 1 is considered prime
+while( milPrimeCount < 1e6 ) {
+    if( milNum.tweakedPrime() ) {
+        milPrimeCount++;
+    }
+    milNum++;
+}
+console.log(`Tweaked: the 1,000,000th prime number is ${milNum-1}`);
+console.log(`Tweaked took ${performance.now() - milStart} milliseconds to run`);
+
+// RESUlTS: 
+// The 1,000,000th prime number is 15.485,863
+// Tweaked took 308492 ms to run
+
+//********************************************************************************
 
 // WHICH IS FASTER - i'm thinking iterative is faster
 // recursive
@@ -57,9 +91,9 @@ function rFib( n ) {
     }
     return rFib( n-1 ) + rFib( n-2 );
 }
-console.time("recursive fib ");
-rFib(20);
-console.timeEnd("recursive fib ");
+console.time("recursive fibonacci took");
+console.log("20th fibonacci is " + rFib(20));
+console.timeEnd("recursive fibonacci took");
 
 // iterative
 function iFib( n ) {
@@ -70,19 +104,22 @@ function iFib( n ) {
     }
     return vals[n];
 }
-console.time("recursive fib ");
-iFib(20);
-console.timeEnd("iterative fib ");
+console.time("iterative fibonacci took");
+console.log("20th fibonacci is " + iFib(20));
+console.timeEnd("iterative fibonacci took");
 
 // RESUlTS: 
-// iterative fib : 1.104ms
-// iterative fib : 0.06ms
+// 20th fibonacci is 6765
+// recursive fibonacci took 1.452 ms
+// iterative fibonacci took 0.127 ms
+
+//********************************************************************************
 
 // REVERSING A STRING
 const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
-console.time("reversing a string with splt(), reverse(), join() ");
+console.time("reversing a string with splt(), reverse(), join() took");
 const reversed = story.split("").reverse().join("");
-console.timeEnd("reversing a string with splt(), reverse(), join() ");
+console.timeEnd("reversing a string with splt(), reverse(), join() took");
 
 // reversing a string with a for loop
 // https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
@@ -95,9 +132,9 @@ function reverseString(str) {
         return rvsStr;
 }
 
-console.time("reversing a string with a for loop ");
+console.time("reversing a string with a for loop took");
 const reversed2 = reverseString(story);
-console.timeEnd("reversing a string with a for loop ");
+console.timeEnd("reversing a string with a for loop took");
 
 // reversing a string with recursion
 // https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb/
@@ -105,11 +142,11 @@ function recRvsStr(str) {
         return (str === '') ? '' : recRvsStr(str.substr(1)) + str.charAt(0);
 }
 
-console.time("reversing a string with recursion ");
+console.time("reversing a string with recursion took");
 const reversed3 = recRvsStr(story);
-console.timeEnd("reversing a string with recursion ");
+console.timeEnd("reversing a string with recursion took");
 
 // RESUlTS: 
-// reversing a string with splt(), reverse(), join() : 0.06ms
-// reversing a string with a for loop : 0.06ms
-// reversing a string with recursion : 0.107ms
+// reversing a string with splt(), reverse(), join() : 0.07ms
+// reversing a string with a for loop : 0.137ms
+// reversing a string with recursion : 0.23ms
