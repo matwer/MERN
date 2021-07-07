2a1c7070a43be8a9c2605c5f0bf27b1e65d48331
Matthew Werner <matwer@outlook.com>
2021-07-07 15:57:12 -0700

    updated for OOP

--- React/prop_it_up/src/App.js
+++ React/prop_it_up/src/App.js
@@ -5,18 +5,26 @@ import Card from './components/Card';
 function App() {
   return (
     <div className="App">
-      <h2 className="card"><Card lastName={"Doe" } />, <Card firstName={"Jane" } /></h2>
-      <h3><Card age={ "45" } /></h3>
-      <h3><Card age={ "Blonde" } /></h3>
-      <h2 className="card"><Card lastName={"Smith" } />, <Card firstName={"John" } /></h2>
-      <h3><Card age={ "88" } /></h3>
-      <h3><Card age={ "White" } /></h3>
-      <h2 className="card"><Card lastName={"Fillmore" } />,<Card firstName={"Millard" } /></h2>
-      <h3><Card age={ "50" } /></h3>
-      <h3><Card age={ "Brown" } /></h3>
-      <h2 className="card"><Card lastName={"Smith" } />,<Card firstName={"Maria" } /></h2>
-      <h3><Card age={ "62" } /></h3>
-      <h3><Card age={ "Silver" } /></h3>
+      <div class="card">
+        <h2><Card lastName={"Doe" } />, <Card firstName={"Jane" } /></h2>
+        <p>Age: <Card age={ "45" } /></p>
+        <p>Hair color: <Card hairColor={ "Blonde" } /></p>
+      </div>
+      <div class="card">
+        <h2><Card lastName={"Smith" } />, <Card firstName={"John" } /></h2>
+        <p>Age: <Card age={ "88" } /></p>
+        <p>Hair color: <Card hairColor={ "White" } /></p>
+      </div>
+      <div class="card">
+        <h2><Card lastName={"Fillmore" } />,<Card firstName={"Millard" } /></h2>
+        <p>Age: <Card age={ "50" } /></p>
+        <p>Hair Color: <Card hairColor={ "Brown" } /></p>
+      </div>
+      <div class="card">
+        <h2><Card lastName={"Smith" } />,<Card firstName={"Maria" } /></h2>
+        <p>Age: <Card age={ "62" } /></p>
+        <p>Hair color: <Card hairColor={ "Silver" } /></p>
+      </div>
     </div>
   );
 }
--- React/prop_it_up/src/components/Card.js
+++ React/prop_it_up/src/components/Card.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			fName: this.props.firstName,
+			lName: this.props.lastName,
+			cAge: this.props.age,
+			hColor: this.props.hairColor
+		}
+	}
+
 	render() {
 		return (
 			<>
-				{this.props.lastName} {this.props.firstName}
-				{this.props.age}
-				{this.props.hairColor}
+				{this.state.lName} 
+				{this.state.fName}
+				{this.state.cAge}
+				{this.state.hColor}
 			</>
 		);
 	}
