348bb757e1f17106e6547d9b5fadbc881170b770
Matthew Werner <matwer@outlook.com>
2021-07-07 10:45:00 -0700

    first jsx :)

---
+++ React/first_jsx/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do:</h3>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the cats</li>
+        <li>Feed the fish</li>
+        <li>Feed the frog</li>
+        <li>Feed the snakes</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
