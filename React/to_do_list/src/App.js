84d0d1e0617e9b294c7db53838d6a4897e729658
Matthew Werner <matwer@outlook.com>
2021-07-13 15:34:47 -0700

    starting coins and to_do_list

---
+++ React/to_do_list/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, {useState} from 'react';
+
+import Notes from './components/Notes';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Notes />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/to_do_list/src/components/Notes.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+const Notes = (props) => {
+	return (
+		<div>
+			<h3>Notes</h3>
+		</div>
+	)
+}
+
+export default Notes;
