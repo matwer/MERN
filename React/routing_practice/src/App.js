ae75fee30a32bf67505081b39b9ae278b0bf8903
Matthew Werner <matwer@outlook.com>
2021-07-28 10:53:05 -0700

    new project

--- React/routing_practice/src/App.js
+++ React/routing_practice/src/App.js
@@ -1,20 +1,30 @@
-import './App.css';
+import "./App.css";
 
-import React from 'react';
-import {Router, Redirect} from '@reach/router';
+import React from "react";
+import { Link, Router, Redirect } from "@reach/router";
 
-import Home from './components/Home';
-import Words from './components/Words';
-import Colors from './components/Colors';
+import Home from "./components/Home";
+import Words from "./components/Words";
+import Colors from "./components/Colors";
 
 function App() {
   return (
     <div className="App">
+      <button>
+        <Link Link to="/home">
+          Home
+        </Link>
+      </button>
+      <button>
+        <Link Link to="/words/:input">
+          Words
+        </Link>
+      </button>
       <Router>
-        <Home path="/home"/>
-        <Redirect from="/" to="/home"  noThrow="true"/>
-        <Words path="home/:input"/>
-        <Colors path="home/:word/:color1/:color2"/>
+        <Home path="/home" />
+        <Redirect from="/" to="/home" noThrow="true" />
+        <Words path="words/:input" />
+        <Colors path="home/:word/:color1/:color2" />
       </Router>
     </div>
   );
